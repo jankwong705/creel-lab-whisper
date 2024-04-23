099da18cc615ea7a5a46c5bb70f55b86770dc8fc
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2024-04-23 13:21:42 -0700

    wav2vec Python

---
+++ Wav2Vec/wav2vec.py
@@ -0,0 +1,66 @@
+import librosa
+import torch
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+import pandas as pd
+import numpy as np
+import os
+
+# Loading Wav2Vec pretrained model
+tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+
+def load_audio(filename, sample_rate=16000):
+    audio, rate = librosa.load(filename, sr = sample_rate)
+    return audio
+
+def transcribe_each(audio):
+    # Taking an input value
+    input_values = tokenizer(audio, return_tensors = "pt").input_values
+
+    # Storing logits (non-normalized prediction values)
+    logits = model(input_values).logits
+
+    # Storing predicted ids
+    prediction = torch.argmax(logits, dim = -1)
+
+    # Passing the prediction to the tokenzer decode to get the transcription
+    transcription = tokenizer.batch_decode(prediction)[0]
+    
+    return transcription
+
+def transcribe(input_folder, output_excel):
+    # List audio files in input folder
+    audio_files = os.listdir(input_folder)
+
+    # Extract actual word
+    actual = input_folder[0:input_folder.rfind('_')]    ## ADJUST AS NEEDED 
+
+    # Create empty datadrame
+    df = pd.DataFrame(columns=['File', 'Actual', 'Predicted'])
+    files = []
+    actuals = []
+    predicted = []
+
+    for audio_file in audio_files:
+        input_audio_file = os.path.join(input_folder, audio_file)
+
+        # Check if it's a wav file (not a folder)
+        if os.path.isfile(input_audio_file) and ".wav" in input_audio_file:
+
+            # Transcribe .wav files 
+            audio = load_audio(input_audio_file)
+            transcription_each = transcribe_each(audio)
+            files.append(input_audio_file)
+            actuals.append(actual)
+            predicted.append(transcription_each)
+
+    # Write to excel file 
+    df['File'] = files
+    df['Actual'] = actuals
+    df['Predicted'] = predicted
+    df.to_excel(output_excel)
+
+folder = input("Enter folder name:")
+output = input("Enter output Excel name:")
+transcribe(folder, output)
+print("All files transcribed.")
