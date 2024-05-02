dae267262cb767f594c13cc1e7d66419c7f5836a
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2024-05-02 12:33:31 -0700

    modified .py

--- Wav2Vec/wav2vec.py
+++ Wav2Vec/wav2vec.py
@@ -44,20 +44,34 @@ def transcribe(input_folder, output_excel):
         # Check if it's a wav file (not a folder)
         if os.path.isfile(input_audio_file) and ".wav" in input_audio_file:
 
-            # Transcribe .wav files 
-            audio = load_audio(input_audio_file)
-            transcription_each = transcribe_each(audio)
-            files.append(input_audio_file)
-            # Extract actual word
-            actual = input_audio_file[input_audio_file.rfind('_')+1:input_audio_file.rfind('.')]    # KIDTALK & AP
-            actuals.append(actual)
-            predicted.append(transcription_each)
+            try: 
+                # Transcribe .wav files 
+                audio = load_audio(input_audio_file)
+                transcription_each = transcribe_each(audio)
+                files.append(input_audio_file)
+
+                # Extract actual word
+                actual = input_audio_file[input_audio_file.rfind('_')+1:input_audio_file.rfind('.')]    # KIDTALK & AP
+
+                actuals.append(actual)
+                predicted.append(transcription_each)
+            except RuntimeError as error:
+                print("Can't transcribe", audio_file)
 
     # Write to excel file 
     df['File'] = files
     df['Actual'] = actuals
     df['Predicted'] = predicted
     df.to_excel(output_excel)
+
+def transcribe_all(folder_of_folders):
+    folders = os.listdir(folder_of_folders)
+
+    for folder in folders:
+        dir = os.path.join(folder_of_folders, folder)
+        if os.path.isdir(dir):
+            excel = folder + ".xlsx"
+            transcribe(dir, excel)
 
 folder = input("Enter folder name:")
 output = input("Enter output Excel name:")
